3302d8eb71be24496110e2dd2765319d3d75a555
berkleybledsoe-afk <berkleybledsoe@gmail.com>
2026-01-02 17:25:44 -0600

    Create Code.py
    Coded in Python

---
+++ Code.py
@@ -0,0 +1,5 @@
+length = int(input("What is the length:"))
+width = int(input("What is the width:"))
+height = int(input("What is the height:"))
+volume = length * width * height
+print(f"The volume of the rectangle is {volume} liters")
